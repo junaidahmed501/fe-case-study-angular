0e2cbab5b3054667802a19d1cc8292648737e433
Junaid <junaidahmed501@gmail.com>
2025-08-08 01:06:26 +0200

    feat: migrated to jest, updated entry component tests
    Signed-off-by: Junaid <junaidahmed501@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+    await TestBed.configureTestingModule({}).compileComponents();
+    // TestBed.overrideTemplate(AppComponent, '');
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
@@ -14,16 +19,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sfe-tech-task' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sfe-tech-task');
+  it(`should have the 'SFE Tech Task' title`, () => {
+    expect(component.title).toEqual('SFE Tech Task');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, sfe-tech-task');
+    expect(compiled.querySelector('h1')?.textContent).toContain('SFE Tech Task');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
-// todo: use the bootstrap component decorator
-// https://angular.io/guide/standalone-components#bootstrapping-standalone-components
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet], // todo: use a core/shared module
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
